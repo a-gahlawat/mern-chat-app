16425bb29f4ff71564ecfed39f090cbd56626131
Ashish Gahlawat <122122547+a-gahlawat@users.noreply.github.com>
2024-06-16 01:38:57 +0530

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { sendMessage, allMessages } = require("../controllers/messageControllers");
+const { sendMessage, allMessages,deleteMessage } = require("../controllers/messageControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage);
 router.route("/:chatId").get(protect, allMessages);
+router.route("/del/:id").delete(protect, deleteMessage);
 
 module.exports = router;
