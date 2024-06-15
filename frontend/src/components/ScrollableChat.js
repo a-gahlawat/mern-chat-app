030fa2bd77046f35fed7ee767221fbe263f7350c
Ashish Gahlawat <122122547+a-gahlawat@users.noreply.github.com>
2024-06-16 01:38:02 +0530

    Update ScrollableChat.js

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -7,15 +7,17 @@ import {
   isSameUser,
 } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
-import { Avatar, Tooltip } from "@chakra-ui/react";
+import { Avatar, Tooltip, IconButton, Box } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
-const ScrollableChat = ({ messages }) => {
+const ScrollableChat = ({ messages, deleteMessage }) => {
   const { user } = ChatState();
+
   return (
-    <ScrollableFeed>
+    <ScrollableFeed style={{ overflowX: "hidden" }}>
       {messages &&
         messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}>
+          <Box display="flex" key={m._id} alignItems="center">
             {(isSameSender(messages, m, i, user._id) ||
               isLastMessage(messages, i, user._id)) && (
               <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
@@ -29,22 +31,30 @@ const ScrollableChat = ({ messages }) => {
                 />
               </Tooltip>
             )}
-
-            <span
-              style={{
-                backgroundColor: `${
-                  m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
-                }`,
-                borderRadius: "20px",
-                padding: "5px 15px",
-                maxWidth: "75%",
-                marginLeft: isSameSenderMargin(messages, m, i, user._id),
-                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
-              }}
+            <Box
+              bg={m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"}
+              borderRadius="20px"
+              padding="5px 15px"
+              maxWidth="75%"
+              ml={isSameSenderMargin(messages, m, i, user._id)}
+              mt={isSameUser(messages, m, i, user._id) ? 3 : 10}
+              position="relative"
             >
               {m.content}
-            </span>
-          </div>
+              {
+                <IconButton
+                  icon={<DeleteIcon />}
+                  size="xs"
+                  onClick={() => deleteMessage(m._id)}
+                  aria-label="Delete message"
+                  position="absolute"
+                  top="-3px"
+                  right="-5px"
+                  variant="ghost"
+                />
+              }
+            </Box>
+          </Box>
         ))}
     </ScrollableFeed>
   );
